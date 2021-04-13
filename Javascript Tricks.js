9dbb0ddc9c03005ed02781e35257d20efd85a76a
cpuuntery <57891661+cpuuntery@users.noreply.github.com>
2021-04-13 12:25:23 +0300

    Add files via upload

--- Javascript Tricks.js
+++ Javascript Tricks.js
@@ -10,25 +10,21 @@ $("div:contains('Example')").remove()
 # select element by attribute
 document.querySelectorAll("[myAttribute='aValue']");
 
-#Extract text from specific html element
+#Extract text from specific html element [change (x.textContent) to suit your needs]
 let elm = document.createElement('textarea');
 elm.setAttribute("id", "output");
 document.querySelector('body').prepend(elm)
-var data = $('#output').text();
-document.querySelectorAll("span.filesize").forEach(function(x) {data += x.textContent + '\n'});
-$('#output').text( data );
+document.querySelectorAll("div.box span a").forEach(x => document.querySelector("#output").textContent += x.textContent + '\n')
 
 
-#Extract text from specific html element with regex if it matches specific text and jquery is required
+#Extract text from specific html element with regex if it matches specific text and [change (x.textContent) to suit your needs]
 let elm = document.createElement('textarea');
 elm.setAttribute("id", "output");
 document.querySelector('body').prepend(elm)
 function get_nodes_containing_text(selector, text) {
 const elements = [...document.querySelectorAll(selector)];
 return elements.filter((element) =>element.childNodes[0] &&element.childNodes[0].nodeValue &&RegExp(text, "i","g","u").test(element.childNodes[0].nodeValue.trim()));}
-var data = $('#output').text();
-get_nodes_containing_text('#filenamelabel', 's05').forEach(function(x) {data += x.textContent + '\n'});
-$('#output').text( data );
+get_nodes_containing_text('a', '~').forEach(x => document.querySelector("#output").textContent += x.textContent + '\n')
 
 
 #Extract text from specific html element if it matches specific text and it is case sensitive and  jquery is required
